30f46304b0057c1d6a468e8dc029008d9adfb499
Skullblz <skullzablaze56@gmail.com>
2025-02-04 18:39:15 -0500

    This is the account creation page (Used in account creation)

---
+++ app/src/main/java/com/example/peakplays/CreateAccountActivity.kt
@@ -0,0 +1,40 @@
+package com.example.peakplays
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.peakplays.databinding.ActivityCreateAccountBinding
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+
+class CreateAccountActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCreateAccountBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCreateAccountBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Hide system UI navigation
+        @Suppress("DEPRECATION")
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+
+        // Setup back button
+        binding.backButton.setOnClickListener {
+            finish()
+        }
+
+        // Setup create account button
+        binding.createAccountButton.setOnClickListener {
+            // Here you would implement account creation logic
+            finish()
+        }
+
+        // Setup country dropdown
+        val countries = resources.getStringArray(R.array.countries)
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, countries)
+        (binding.countryInput as? AutoCompleteTextView)?.setAdapter(arrayAdapter)
+    }
+} 
